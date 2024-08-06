7f58466a13e88535ffc36b83e2f783fc38cf6374
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-06 15:04:51 +0200

    feat: add fetching function to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,49 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import { Outlet } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+const useData = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products?limit=0', { mode: 'cors' })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('server error');
+        }
+        return response.json();
+      })
+      .then((response) =>
+        setData(
+          response.products.filter((item) => {
+            return (
+              item.tags.includes('clothing') ||
+              item.tags.includes('home decor') ||
+              item.tags.includes('sunglasses')
+            );
+          })
+        )
+      )
+      .catch((error) => setError(error.message))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return { data, error, loading };
+};
 
 function App() {
+  const { data, error, loading } = useData();
   return (
-    <>
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </>
+    !loading && (
+      <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </>
+    )
   );
 }
 
