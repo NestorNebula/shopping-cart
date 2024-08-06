b3529ba06d9f9ec12cb86627f48a9efd538f831b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-06 16:58:20 +0200

    feat: add Shop component

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,3 +1,27 @@
-function Shop() {}
+import { useOutletContext } from 'react-router-dom';
+
+function Shop() {
+  const { data } = useOutletContext();
+
+  return (
+    <main>
+      <header>
+        <h2>Fake Store's Shop</h2>
+      </header>
+      <div>{data.length} Items</div>
+      <section>
+        {data.map((item) => {
+          return (
+            <div data-testid="item" className="item" key={item.id}>
+              <img src={item.images[0]} alt="" />
+              <div>{item.title}</div>
+              <div>{item.price}$</div>
+            </div>
+          );
+        })}
+      </section>
+    </main>
+  );
+}
 
 export default Shop;
