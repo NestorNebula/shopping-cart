4c2b6f189d08e200f0d2e7f92c451bb9a2f0decd
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-06 11:00:13 +0200

    feat: add Navbar with Links to the App

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,21 @@
-function Navbar() {}
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="shop">Shop</Link>
+        </li>
+        <li>
+          <Link to="/">Fake Store</Link>
+        </li>
+        <li>
+          <Link to="cart">Cart</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
 export default Navbar;
