2c9f952a431e9a748a93065ba7195941f40935da
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-06 11:16:21 +0200

    feat: add Homepage basis HTML

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,3 +1,20 @@
-function Homepage() {}
+import { Link } from 'react-router-dom';
+
+function Homepage() {
+  return (
+    <main>
+      <header>
+        <div>Fake Store</div>
+      </header>
+      <section>
+        <div>
+          <div>Start shopping now!</div>
+          <Link to="shop">Shop</Link>
+          <Link to="cart">Cart</Link>
+        </div>
+      </section>
+    </main>
+  );
+}
 
 export default Homepage;
