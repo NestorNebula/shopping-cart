6f7d64a932246627ea5cf95309b646366638347a
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-07 16:32:26 +0200

    refactor: update tests following Cart rework

--- tests/Card.test.js
+++ tests/Card.test.js
@@ -3,32 +3,41 @@ import { FakeData } from '../src/placeholders/FakeData';
 import { Cart } from '../src/Cart';
 
 const data = FakeData().data;
+const mockSetItems = vi.fn((update) => update);
 
 describe('getTotal', () => {
-  const cart = Cart();
+  let cart = Cart([], mockSetItems);
   it('returns 0 when nothing in Cart', () => {
     expect(cart.getTotal()).toBe(0);
   });
 
   it('returns total when items inside Cart', () => {
     cart.addItem(data[0], 2);
+    const newItems = mockSetItems.mock.lastCall[0];
+    cart = Cart(newItems, mockSetItems);
     expect(cart.getTotal()).toBe(119.98);
   });
 });
 
 describe('addItem', () => {
-  const cart = Cart();
+  let cart = Cart([], mockSetItems);
   it('add an item correctly', () => {
     cart.addItem(data[0], 1);
+    const newItems = mockSetItems.mock.lastCall[0];
+    cart = Cart(newItems, mockSetItems);
     expect(cart.getTotal()).toBe(59.99);
   });
 });
 
 describe('clearCart', () => {
-  const cart = Cart();
+  let cart = Cart([], mockSetItems);
   it('remove all items from cart', () => {
     cart.addItem(data[0], 1);
+    let newItems = mockSetItems.mock.lastCall[0];
+    cart = Cart(newItems, mockSetItems);
     cart.clearCart();
+    newItems = mockSetItems.mock.lastCall[0];
+    cart = Cart(newItems, mockSetItems);
     expect(cart.getTotal()).toBe(0);
   });
 });
