370475e0015e8b45bc20ba4bfbea99d2024e2875
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-07 10:24:56 +0200

    test: update tests to support Link import

--- tests/Shop.test.jsx
+++ tests/Shop.test.jsx
@@ -3,32 +3,48 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Shop from '../src/components/shop/Shop';
 import { FakeData } from '../src/placeholders/FakeData';
+import { MemoryRouter } from 'react-router-dom';
 
 const data = FakeData().data;
 
-vi.mock('react-router-dom', () => ({
-  ...vi.importActual('react-router-dom'),
-  useOutletContext: () => ({
-    data: data,
-  }),
-}));
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+  return {
+    ...actual,
+    useOutletContext: vi.fn(() => {
+      return { data: data };
+    }),
+  };
+});
 
 describe('Shop', () => {
   it('renders correctly', () => {
-    render(<Shop />);
+    render(
+      <MemoryRouter>
+        <Shop />
+      </MemoryRouter>
+    );
     const header = screen.getByRole('heading');
     expect(header.textContent).toMatch("Fake Store's Shop");
   });
 
   it('display all items', () => {
-    render(<Shop />);
+    render(
+      <MemoryRouter>
+        <Shop />
+      </MemoryRouter>
+    );
     const items = screen.queryAllByTestId('item');
     expect(items.length).toEqual(data.length);
   });
 
   it('display items based on search', async () => {
     const user = userEvent.setup();
-    render(<Shop />);
+    render(
+      <MemoryRouter>
+        <Shop />
+      </MemoryRouter>
+    );
     const searchbar = screen.getByLabelText('Search Item');
     await user.type(searchbar, 'house');
     const items = screen.queryAllByTestId('item');
