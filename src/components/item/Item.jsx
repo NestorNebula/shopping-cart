06ce761e2b9614bb19814b53dabdbc451ced111c
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-07 11:23:29 +0200

    feat: add basis Item Component

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,3 +1,33 @@
-function Item() {}
+import { useOutletContext, Link, useParams } from 'react-router-dom';
+import { useState } from 'react';
+
+function Item() {
+  const { data, cart } = useOutletContext();
+  const { item: id } = useParams();
+  const [quantity, setQuantity] = useState(1);
+  const regex = new RegExp('([0-9]+)', 'i');
+
+  const updateQuantity = (e) => {
+    if (regex.test(e.target.value)) setQuantity(e.target.value);
+  };
+  const item = data.find((itm) => itm.id === +id);
+
+  return (
+    <div>
+      <img src={item.images[0]} alt="" />
+      <div>{item.title}</div>
+      <div>{item.price}$</div>
+      <div>{item.description}</div>
+      <button>Add Item</button>
+      <label htmlFor="quantity"></label>
+      <input
+        id="quantity"
+        type="number"
+        value={quantity}
+        onChange={updateQuantity}
+      />
+    </div>
+  );
+}
 
 export default Item;
