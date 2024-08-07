1a2cbcca85c932860d6c50c1b8ff47cd5f18ea6e
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-07 17:15:28 +0200

    feat: create Cart component and fix addItem func

--- src/Cart.js
+++ src/Cart.js
@@ -5,10 +5,8 @@ export function Cart(itm, setItems) {
     items.reduce((sum, item) => (sum += item.item.price * item.quantity), 0);
 
   const addItem = (item, quantity) => {
-    items.forEach((cartItem) => {
-      if (cartItem.item.id === cartItem.id) return false;
-    });
-    setItems([...items, { item: item, quantity: quantity }]);
+    !items.some((cartItem) => cartItem.item.id === item.id) &&
+      setItems([...items, { item: item, quantity: quantity }]);
   };
 
   const clearCart = () => setItems([]);
--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,3 +1,31 @@
-function Cart() {}
+import { useOutletContext, Link } from 'react-router-dom';
+
+function Cart() {
+  const { data, cart } = useOutletContext();
+
+  return (
+    <section>
+      <div>Your Cart</div>
+      <div>
+        <div>Item</div>
+        <div>Price</div>
+        <div>Quantity</div>
+      </div>
+      {cart.items.map((item) => {
+        return (
+          <div key={item.item.id}>
+            <div>{item.item.title}</div>
+            <div>{item.item.price}</div>
+            <div>{item.quantity}</div>
+          </div>
+        );
+      })}
+      <div>
+        <div>Total:</div>
+        <div>{cart.getTotal()}$</div>
+      </div>
+    </section>
+  );
+}
 
 export default Cart;
