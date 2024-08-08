52729bfe6ad8befc3f6329c4508cb4faed1c9ad0
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-08 15:44:22 +0200

    style: add styles and more content to Item

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,5 +1,6 @@
 import { useOutletContext, Link, useParams } from 'react-router-dom';
 import { useState } from 'react';
+import styles from './Item.module.css';
 
 function Item() {
   const { data, cart } = useOutletContext();
@@ -11,41 +12,64 @@ function Item() {
     if (regex.test(e.target.value)) setQuantity(e.target.value);
   };
   const item = data.find((itm) => itm.id === +id);
+  const reviews = item.reviews;
   const addToCart = () => {
     cart.addItem(item, quantity || '1');
   };
 
   return (
-    <div>
-      <div>
+    <main className={styles.main}>
+      <div className={styles.item}>
         <img src={item.images[0]} alt="" />
-        <div>{item.title}</div>
-        <div>{item.price}$</div>
-        <div>{item.description}</div>
+        <div className={styles.itemInfos}>
+          {cart.items.some((cartItem) => cartItem.item.id === item.id) ? (
+            <div className={styles.addedToCart}>
+              <div>This item has been added to your cart!</div>
+              <Link to="/cart">
+                <button>Check your cart</button>
+              </Link>
+            </div>
+          ) : (
+            <div className={styles.addToCart}>
+              <button className={styles.addButton} onClick={addToCart}>
+                Add to Cart
+              </button>
+              <label htmlFor="quantity">Quantity:</label>
+              <input
+                className={styles.quantityInput}
+                id="quantity"
+                type="number"
+                placeholder="1"
+                value={quantity}
+                onChange={updateQuantity}
+              />
+            </div>
+          )}
+          <div className={styles.itemTitle}>{item.title}</div>
+          <div className={styles.itemPrice}>{item.price}$</div>
+          <div className={styles.itemDesc}>{item.description}</div>
+          <div className={styles.reviewsTitle}>Reviews:</div>
+          <div className={styles.reviews}>
+            {reviews.map((review) => {
+              return (
+                <div
+                  className={styles.review}
+                  key={review.reviewerName + review.comment}
+                >
+                  <div>{review.reviewerName}</div>
+                  <div>{review.rating} / 5</div>
+                  <div>"{review.comment}"</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
-      {cart.items.some((cartItem) => cartItem.item.id === item.id) ? (
-        <>
-          <div>This item has been added to your cart!</div>
-          <Link to="/cart">Check your cart</Link>
-        </>
-      ) : (
-        <>
-          <button onClick={addToCart}>Add to Cart</button>
-          <label htmlFor="quantity">Quantity:</label>
-          <input
-            id="quantity"
-            type="number"
-            placeholder="1"
-            value={quantity}
-            onChange={updateQuantity}
-          />
-        </>
-      )}
-      <div>
+      <div className={styles.links}>
         <Link to="/">Homepage</Link>
         <Link to="/shop">Shop</Link>
       </div>
-    </div>
+    </main>
   );
 }
 
