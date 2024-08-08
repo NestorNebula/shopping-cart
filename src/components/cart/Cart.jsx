bed510b64fa47b58307f046f7e29f00dc25cd2d3
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-08 16:29:41 +0200

    style: add styles and content to Cart component

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,5 +1,6 @@
-import { useOutletContext, Navigate } from 'react-router-dom';
+import { useOutletContext, Navigate, Link } from 'react-router-dom';
 import { useState } from 'react';
+import styles from './Cart.module.css';
 
 function Cart() {
   const { cart } = useOutletContext();
@@ -10,30 +11,46 @@ function Cart() {
     setSubmit(true);
   };
 
-  return (
-    <section>
-      {submit && <Navigate to="/" />}
-      <div>Your Cart</div>
-      <div>
-        <div>Item</div>
-        <div>Price</div>
-        <div>Quantity</div>
-      </div>
-      {cart.items.map((item) => {
-        return (
-          <div key={item.item.id}>
-            <div>{item.item.title}</div>
-            <div>{item.item.price}</div>
-            <div>{item.quantity}</div>
+  return cart.items.length < 1 ? (
+    <main className={styles.main}>
+      <section className={styles.noCart}>
+        <div>You don't have any item in your cart for now!</div>
+        <Link className={styles.returnButton} to="/shop">
+          Return to shop
+        </Link>
+      </section>
+    </main>
+  ) : (
+    <main className={styles.main}>
+      <section className={styles.cart}>
+        {submit && <Navigate to="/" />}
+        <div>Your Cart</div>
+        <div className={styles.rows}>
+          <div className={styles.cartHeaders}>
+            <div>Item</div>
+            <div>Price</div>
+            <div>Quantity</div>
           </div>
-        );
-      })}
-      <div>
-        <div>Total:</div>
-        <div>{cart.getTotal()}$</div>
-      </div>
-      <button onClick={handleFakeOrder}>Confirm order</button>
-    </section>
+          {cart.items.map((item) => {
+            return (
+              <div className={styles.item} key={item.item.id}>
+                <img src={item.item.images[0]} alt="/" />
+                <div>{item.item.title}</div>
+                <div>{item.item.price}</div>
+                <div>{item.quantity}</div>
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          <div>Total:</div>
+          <div>{Math.round(cart.getTotal() * 100) / 100}$</div>
+        </div>
+        <button className={styles.confirmOrder} onClick={handleFakeOrder}>
+          Confirm order
+        </button>
+      </section>
+    </main>
   );
 }
 
