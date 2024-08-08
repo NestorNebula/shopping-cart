843c432952a18198b00d24a3b5638481ed52de33
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-08 13:33:33 +0200

    style: add styles and animations to the homepage

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,14 +1,39 @@
-import { Link } from 'react-router-dom';
+import { Link, useOutletContext } from 'react-router-dom';
+import styles from './Homepage.module.css';
 
 function Homepage() {
+  const { data } = useOutletContext();
+  const reversedData = [];
+  for (let i = data.length - 1; i >= 0; i--) {
+    reversedData.push(data[i]);
+  }
+
   return (
-    <main>
-      <header>
-        <div>Fake Store</div>
+    <main className={styles.homepage}>
+      <header className={styles.title}>
+        <div>Welcome to Fake Store!</div>
       </header>
-      <section>
-        <div>
-          <div>Start shopping now!</div>
+      <section className={styles.section}>
+        <div className={styles.sectionTitle}>Start shopping now!</div>
+        <div className={styles.items}>
+          <div className={styles.itemsContainer}>
+            {data.map((item) => (
+              <div key={item.id + '1'} className={styles.imgContainer}>
+                <img className={styles.img} src={item.images[0]} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={styles.items}>
+          <div className={styles.itemsContainerBottom}>
+            {reversedData.map((item) => (
+              <div key={item.id + '2'} className={styles.imgContainer}>
+                <img className={styles.img} src={item.images[0]} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={styles.links}>
           <Link to="shop">Shop</Link>
           <Link to="cart">Cart</Link>
         </div>
