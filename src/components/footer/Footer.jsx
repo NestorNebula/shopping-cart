421e369dd17988875610f159a5d93de643f6110d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-09 15:25:42 +0200

    feat: add content to the Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,36 @@
+import styles from './Footer.module.css';
+
 function Footer() {
-  return <footer></footer>;
+  return (
+    <footer>
+      <div className={styles.credits}>
+        <a
+          href="https://www.flaticon.com/free-icons/shopping-bag"
+          title="shopping-bag icons"
+        >
+          Shopping-bag icons created by iconixar - Flaticon
+        </a>
+        <a
+          href="https://www.flaticon.com/free-icons/shopping-cart"
+          title="shopping cart icons"
+        >
+          Shopping cart icons created by Freepik - Flaticon
+        </a>
+        <a
+          href="https://www.flaticon.com/free-icons/homepage"
+          title="homepage icons"
+        >
+          Homepage icons created by Smashicons - Flaticon
+        </a>
+        <a href="https://www.flaticon.com/free-icons/star" title="star icons">
+          Star icons created by Pixel perfect - Flaticon
+        </a>
+        <a href="https://pictogrammers.com/library/mdi/">
+          Cart icon from Material Design Icons - Google
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
