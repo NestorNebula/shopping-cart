8d45e281bf0541f51bab10d5721e433e0f79f277
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-09 10:30:19 +0200

    test: add new test for Remove button

--- tests/Cart.test.jsx
+++ tests/Cart.test.jsx
@@ -45,4 +45,17 @@ describe('cart', () => {
     await user.click(button);
     expect(cart.clearCart).toHaveBeenCalledOnce();
   });
+
+  it('calls removeItem when remove button clicked', async () => {
+    cart.removeItem = vi.fn();
+    const user = userEvent.setup();
+    render(
+      <MemoryRouter>
+        <Cart />
+      </MemoryRouter>
+    );
+    const button = screen.getByRole('button', { name: 'Remove' });
+    await user.click(button);
+    expect(cart.removeItem).toHaveBeenCalledWith(43);
+  });
 });
