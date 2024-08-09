4318fcceebe846b49f3c66b50f9ea951c8312971
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-09 16:12:44 +0200

    feat: check that the error status is 404

--- src/components/error/ErrorPage.jsx
+++ src/components/error/ErrorPage.jsx
@@ -1,11 +1,17 @@
-import { Link } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
+import styles from './ErrorPage.module.css';
 
 function ErrorPage() {
+  const error = useRouteError();
   return (
-    <div>
-      <h1>Error</h1>
-      <div>Sorry, it seems you landed on a page that does'nt exist!</div>
-      <Link to="/">Return to Homepage</Link>
+    <div className={styles.errorPage}>
+      <h1>Error {error.status}</h1>
+      {error.status === 404 && (
+        <div>Sorry, it seems you landed on a page that does'nt exist!</div>
+      )}
+      <Link className={styles.returnLink} to="/">
+        Return to Homepage
+      </Link>
     </div>
   );
 }
