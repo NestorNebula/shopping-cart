30c1f6f1c0938a058614cdb54ed76d250cfaaaac
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-08-09 15:57:04 +0200

    build: add ErrorPage to the route

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,11 +3,13 @@ import Homepage from '../components/homepage/Homepage';
 import Shop from '../components/shop/Shop';
 import Item from '../components/item/Item';
 import Cart from '../components/cart/Cart';
+import ErrorPage from '../components/error/ErrorPage';
 
 const routes = [
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Homepage /> },
       { path: 'shop', element: <Shop /> },
