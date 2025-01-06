4c34d1e6a349bd6be60248d76042db28c182a03c
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-06 18:06:49 +0100

    refactor: change App language to TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
+import { useData } from './hooks/useData';
+import { Cart } from './Cart';
+
+function App() {
+  const { data, error, loading } = useData();
+  const [items, setItems] = useState([]);
+  const cart = Cart(items, setItems);
+  return (
+    !loading &&
+    !error && (
+      <>
+        <Navbar cart={cart} />
+        <Outlet context={{ data, cart }} />
+        <Footer />
+      </>
+    )
+  );
+}
+
+export default App;
