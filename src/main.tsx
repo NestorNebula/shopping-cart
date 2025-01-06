6114c8f0438f5dc7b7f4964f1f7974574bdb9f13
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-06 17:18:11 +0100

    refactor: change language to TypeScript

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 const router = createBrowserRouter(routes);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
