1da9aa850ca56253f221c7fa151c1156a7ffb259
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-06 18:05:08 +0100

    feat: add Review and expand Item interfaces

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,21 @@
 interface Item {
-  brand?: string,
+  id: number,
+  title: string,
+  description: string,
   category: string,
+  price: number,
+  rating: number,
+  brand?: string,
+  reviews: Review,
+  images: any[],
+}
+
+interface Review {
+  rating: number,
+  comment: string,
+  date: Date,
+  reviewerName: string,
+
 }
 
 export type { Item };
