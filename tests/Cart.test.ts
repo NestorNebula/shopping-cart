e689c267b149713b3b70efabff0f0386ee9a9d1d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-08 10:02:30 +0100

    refactor: change language to TypeScript

---
+++ tests/Cart.test.ts
@@ -0,0 +1,57 @@
+import { vi, describe, it, expect } from 'vitest';
+import { FakeData } from './FakeData';
+import type { Item } from '../src/types/types';
+import { Cart } from '../src/Cart';
+
+const data: Item[] = FakeData().data;
+const mockSetItems = vi.fn((update) => update);
+
+describe('getTotal', () => {
+  let cart = Cart([], mockSetItems);
+  it('returns 0 when nothing in Cart', () => {
+    expect(cart.getTotal()).toBe(0);
+  });
+
+  it('returns total when items inside Cart', () => {
+    cart.addItem(data[0], 2);
+    expect(mockSetItems).toHaveBeenCalledOnce();
+    const newItems = mockSetItems.mock.lastCall![0];
+    cart = Cart(newItems, mockSetItems);
+    expect(cart.getTotal()).toBe(119.98);
+  });
+});
+
+describe('addItem', () => {
+  let cart = Cart([], mockSetItems);
+  it('add an item correctly', () => {
+    cart.addItem(data[0], 1);
+    expect(mockSetItems).toHaveBeenCalledOnce();
+    const newItems = mockSetItems.mock.lastCall![0];
+    cart = Cart(newItems, mockSetItems);
+    expect(cart.getTotal()).toBe(59.99);
+  });
+});
+
+describe('clearCart', () => {
+  let cart = Cart([], mockSetItems);
+  it('remove all items from cart', () => {
+    cart.addItem(data[0], 1);
+    expect(mockSetItems).toHaveBeenCalledOnce();
+    let newItems = mockSetItems.mock.lastCall![0];
+    cart = Cart(newItems, mockSetItems);
+    cart.clearCart();
+    expect(mockSetItems).toHaveBeenCalledTimes(2);
+    newItems = mockSetItems.mock.lastCall![0];
+    cart = Cart(newItems, mockSetItems);
+    expect(cart.getTotal()).toBe(0);
+  });
+});
+
+describe('removeItem', () => {
+  let cart = Cart([], mockSetItems);
+  it('remove an item from cart', () => {
+    cart.addItem(data[0], 1);
+    cart.removeItem(data[0].id);
+    expect(cart.items.length).toBe(0);
+  });
+});
