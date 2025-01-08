d54260f8f6eb31107447589cb74449671e074c89
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-08 10:02:55 +0100

    feat: update items state's type

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import Footer from './components/footer/Footer';
 import { Outlet } from 'react-router-dom';
 import { useState } from 'react';
 import { useData } from './hooks/useData';
+import type { Item } from './types/types';
 import { Cart } from './Cart';
 
 function App() {
   const { data, error, loading } = useData();
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState<{ item: Item; quantity: number }[]>([]);
   const cart = Cart(items, setItems);
   return (
     !loading &&
