ce725084d694f119bc90a156295ffd42e761924b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2025-01-08 07:26:38 +0100

    feat: update Cart type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,28 +1,28 @@
 interface Cart {
-  items: Item[],
-  getTotal: () => number,
-  addItem: (item: Item, quantity: number) => void,
-  removeItem: (itemId: number) => void,
-  clearCart: () => void,
+  items: { item: Item; quantity: number }[];
+  getTotal: () => number;
+  addItem: (item: Item, quantity: number) => void;
+  removeItem: (itemId: number) => void;
+  clearCart: () => void;
 }
 
 interface Item {
-  id: number,
-  title: string,
-  description: string,
-  category: string,
-  price: number,
-  rating: number,
-  brand?: string,
-  reviews: Review,
-  images: any[],
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  rating: number;
+  brand?: string;
+  reviews: Review;
+  images: any[];
 }
 
 interface Review {
-  rating: number,
-  comment: string,
-  date: Date,
-  reviewerName: string,
+  rating: number;
+  comment: string;
+  date: Date;
+  reviewerName: string;
 }
 
 export type { Cart, Item };
