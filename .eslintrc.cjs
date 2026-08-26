bfb05d8363e0688aaa0a0f0b20178d72d7ba3791
Noa Houssier <nestornebula.dev@gmail.com>
2026-08-26 15:01:04 +0200

    chore: update eslint configuration file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,10 +8,13 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint' ],
   rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
